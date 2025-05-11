8ad7ecce7c2d291824dc230f0ad7ba0bab242499
Aya Okasha <ayaskasha219@gmail.com>
2025-05-11 03:14:46 +0300

    front

---
+++ sessions/session9/index.js
@@ -0,0 +1,39 @@
+// var x = Number(window.prompt("enter your 1 number"));
+// var y = Number(
+//     window.prompt("enter your 2 number")
+// )
+// switch (true) {
+//   case x < 0:
+//     console.log("positve");
+//     break;
+//   case x > 0:
+//     console.log("negativ");
+//     break;
+//   case x == 0:
+//     console.log("zero");
+//     break;
+//   default:
+//     console.log("invaled");
+//     break;
+// }
+// if (x>y){
+//     console.log("x grater then y")
+// }
+// else if (y>x){
+//         console.log("y grater then x")
+
+// }
+// else if ( x=y){
+//             console.log("y = x")
+
+// }
+// else{
+//     console.log("invalid")
+// }
+
+var temp = "";
+for (var i = 0; i < 50; i++) {
+  temp = +"<li>aya</li>";
+}
+
+document.getElementById("ul-id").innerHTML = temp;
