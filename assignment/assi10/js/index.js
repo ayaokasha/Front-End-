e6a943bb1a0c531914f2ccae64c8a7aad11c493b
Aya Okasha <ayaskasha219@gmail.com>
2025-06-15 14:22:05 +0300

    front

--- assignment/assi10/js/index.js
+++ assignment/assi10/js/index.js
@@ -9,23 +9,43 @@ if (localStorage.getItem("allBooks")) {
 }
 //^add====================
 function addBook() {
-  var books = {
-    name: bookInputName.value,
-    url: bookInputUrl.value,
-  };
+  if (
+    isValid(inputRegex.name, bookInputName) &&
+    isValid(inputRegex.url, bookInputUrl)
+  ) {
+    var books = {
+      name: bookInputName.value,
+      url: bookInputUrl.value,
+    };
 
-  bookList.push(books);
+    bookList.push(books);
 
-  console.log(bookList);
+    console.log(bookList);
 
-  localStorage.setItem("allBooks", JSON.stringify(bookList));
+    localStorage.setItem("allBooks", JSON.stringify(bookList));
 
-  displayBooks(bookList);
-  resetBook();
+    displayBooks(bookList);
+    resetBook();
+    Swal.fire({
+      icon: "success",
+      title: "Book is Added!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill all fields correctly!",
+    });
+  }
 }
 function resetBook() {
   bookInputName.value = "";
   bookInputUrl.value = "";
+
+  bookInputName.classList.remove("is-valid", "is-invalid");
+  bookInputUrl.classList.remove("is-valid", "is-invalid");
 }
 
 //^display====================
@@ -72,3 +92,47 @@ function deleteBook(index) {
 }
 
 //^validation================
+
+var inputRegex = {
+  name: /^[a-zA-Z\s.]{2,}$/,
+  url: /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)(\/[\w- .\/?%&=]*)?$/,
+};
+
+function isValid(regex, input) {
+  if (regex.test(input.value)) {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+
+    input.nextElementSibling.classList.replace("d-none", "d-block");
+    input.nextElementSibling.classList.replace("d-block", "d-none");
+    return true;
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+
+    input.nextElementSibling.classList.replace("d-block", "d-none");
+    input.nextElementSibling.classList.replace("d-none", "d-block");
+
+    return false;
+  }
+}
+
+//^dark mode====================
+var toggleBtn = document.getElementById("toggleModeBtn");
+var body = document.body;
+
+// Check localStorage for last mode
+if (localStorage.getItem("mode") === "dark") {
+  body.classList.add("dark-mode");
+} else {
+  body.classList.add("light-mode");
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+
+  // Save the mode
+  var mode = body.classList.contains("dark-mode") ? "dark" : "light";
+  localStorage.setItem("mode", mode);
+});
